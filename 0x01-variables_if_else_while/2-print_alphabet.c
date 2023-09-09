8fd31062759cba2337d8c48a339caa183734be19
Rehoboth princewill <Rehoprincewill@gmail.com>
2023-09-08 21:53:19 -0700

    Added 2 Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Description: prints the letters in the alphabet.
+ *
+ * Return: Always O (Success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);	
+}
