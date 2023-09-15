2a08f9b5f42a5d3ef83b6acf1bada7adb54d4cf4
Rehoboth princewill <Rehoprincewill@gmail.com>
2023-09-14 21:09:44 -0700

    Added 4 Print Most Numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints digits using _putchar
+* except digits 2 and 4
+*
+*/
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' || c == '4')
+			continue;
+		_putchar(c);
+	}
+	_putchar('\n');
+}
