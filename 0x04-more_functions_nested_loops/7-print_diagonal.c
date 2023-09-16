1a09f36dbafe531845aead7efb3eb30ea5691c16
Rehoboth princewill <Rehoprincewill@gmail.com>
2023-09-15 17:54:04 -0700

    Added 7 Print Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_diagonal - draws a diagonal line
+* by repeating \ n times
+*
+* @n: number of rows to draw line
+*
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
