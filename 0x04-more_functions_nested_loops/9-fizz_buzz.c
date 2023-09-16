adcd85bc1932ab3c46f4db390d66eae864337e23
Rehoboth princewill <Rehoprincewill@gmail.com>
2023-09-15 19:07:53 -0700

    Added 9 Fizz Bizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100 but replaces every
+ * multple of 3 with 'Fizz', 5 with 'Bizz', and
+ * 15 with 'FizzBizz'
+ *
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	int i;
+	int num = 101;
+
+	for (i = 1; i < num; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Bizz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
